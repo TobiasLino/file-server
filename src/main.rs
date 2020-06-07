7023b3a1bb1965ceb5e2a1e2205edc26c97804ca
TobiasLino <tobias.lino@fatec.sp.gov.br>
2020-06-07 11:59:49 -0300

    server: handle the connection

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-    let mut x = String::from("Tobias");
-    let mut y = x.clone();
-    println!("x: {}, y: {}", x, y);
-    y = String::from("ana");
-    println!("x: {}, y: {}", x, y);
-}
